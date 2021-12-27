a60c8e0cc37fed6e434cd52d9d5e80c255b14d5e
PabloGarcia123 <93787897+PabloGarcia123@users.noreply.github.com>
2021-12-27 21:42:51 +0100

    Add files via upload

---
+++ Access/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace ProyectoIPO2020_2021.Access
+{
+    public class Conexion
+    {
+        protected OleDbConnection conexion;
+
+        protected OleDbConnection ConexionBD
+        {
+            get
+            {
+                conexion = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=BBDDPerros.accdb;Persist Security Info=True");
+                conexion.Open();
+                return conexion;
+            }
+        }
+    }
+}
