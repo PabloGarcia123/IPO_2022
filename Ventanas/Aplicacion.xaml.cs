b0d7bc0f9adcf4a3c5aaa2e8d4a3b1a4e317b692
NoelFlajelo12 <91826014+NoelFlajelo12@users.noreply.github.com>
2021-12-30 15:40:56 +0100

    Actualización Aplicación en pestaña Voluntarios

--- Ventanas/Aplicacion.xaml.cs
+++ Ventanas/Aplicacion.xaml.cs
@@ -12,8 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Win32;
-using System.Data.OleDb;
-using Oracle.DataAccess;
+using System.IO;
+
 
 namespace ProyectoIPO2020_2021
 {
@@ -30,8 +30,7 @@ namespace ProyectoIPO2020_2021
         private static BitmapImage imgUsuario1 = new BitmapImage(new Uri("/Images/Mike-Wazowski.jpg", UriKind.Relative));
         private static Usuario usuario1 = new Usuario("Mario Torres Toledano", "Mario.Torres@gmail.com", "05285136F", 688455782, DateTime.Now, imgUsuario1, "123");
         
-        public static OleDbConnection conectar = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\05980124\\OneDrive\\Uni\\Vs2019Projects\\ProyectoIPO2020-2021\\ProyectoIPO2020-2021\\BasesDeDatos\\BBDDPerros.accdb");
-        OleDbCommand comando = new OleDbCommand();
+        
         
         public Aplicacion()
         {
@@ -94,15 +93,108 @@ namespace ProyectoIPO2020_2021
 
         private void btnAñadir_Click(object sender, RoutedEventArgs e)
         {
-         
-           
+            StreamWriter escribir = new StreamWriter("TablaPerros.txt");
+            escribir.WriteLine(txtboxNombrePerro.Text + " " + txtboxRazaPerro.Text);
+            escribir.Close();
         }
 
-
-
-        private void btnListar_Click(object sender, RoutedEventArgs e)
+        private void CheckBoxLunes_Checked(object sender, RoutedEventArgs e)
         {
-            
+            CheckBoxLunesMa.IsEnabled = true;
+            CheckBoxLunesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxMartes_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMartesMa.IsEnabled = true;
+            CheckBoxMartesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxMiercoles_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMiercolesMa.IsEnabled = true;
+            CheckBoxMiercolesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxJueves_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxJuevesMa.IsEnabled = true;
+            CheckBoxJuevesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxViernes_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxViernesMa.IsEnabled = true;
+            CheckBoxViernesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxSabado_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxSabadoMa.IsEnabled = true;
+            CheckBoxSabadoTar.IsEnabled = true;
+        }
+
+        private void CheckBoxDomingo_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxDomingoMa.IsEnabled = true;
+            CheckBoxDomingoTar.IsEnabled = true;
+        }
+
+        private void CheckBoxDomingo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxDomingoMa.IsEnabled = false;
+            CheckBoxDomingoTar.IsEnabled = false;
+            CheckBoxDomingoMa.IsChecked = false;
+            CheckBoxDomingoTar.IsChecked = false;
+
+        }
+
+        private void CheckBoxSabado_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxSabadoMa.IsEnabled = false;
+            CheckBoxSabadoTar.IsEnabled = false;
+            CheckBoxSabadoMa.IsChecked = false;
+            CheckBoxSabadoTar.IsChecked = false;
+        }
+
+        private void CheckBoxViernes_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxViernesMa.IsEnabled = false;
+            CheckBoxViernesTar.IsEnabled = false;
+            CheckBoxViernesMa.IsChecked = false;
+            CheckBoxViernesTar.IsChecked = false;
+        }
+
+        private void CheckBoxJueves_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxJuevesMa.IsEnabled = false;
+            CheckBoxJuevesTar.IsEnabled = false;
+            CheckBoxJuevesMa.IsChecked = false;
+            CheckBoxJuevesTar.IsChecked = false;
+        }
+
+        private void CheckBoxMiercoles_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMiercolesMa.IsEnabled = false;
+            CheckBoxMiercolesTar.IsEnabled = false;
+            CheckBoxMiercolesMa.IsChecked = false;
+            CheckBoxMiercolesTar.IsChecked = false;
+        }
+
+        private void CheckBoxMartes_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMartesMa.IsEnabled = false;
+            CheckBoxMartesTar.IsEnabled = false;
+            CheckBoxMartesMa.IsChecked = false;
+            CheckBoxMartesTar.IsChecked = false;
+        }
+
+        private void CheckBoxLunes_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxLunesMa.IsEnabled = false;
+            CheckBoxLunesTar.IsEnabled = false;
+            CheckBoxLunesMa.IsChecked = false;
+            CheckBoxLunesTar.IsChecked = false;
         }
     }
 }
