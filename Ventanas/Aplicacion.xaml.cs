e251baf6c62725cff4fbf7eb33d52fabbd599903
NoelFlajelo12 <91826014+NoelFlajelo12@users.noreply.github.com>
2022-01-04 17:05:09 +0100

    Add files via upload

--- Ventanas/Aplicacion.xaml.cs
+++ Ventanas/Aplicacion.xaml.cs
@@ -12,8 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Win32;
-using System.Data.OleDb;
-using Oracle.DataAccess;
+using System.IO;
+using ProyectoIPO2020_2021.Clases;
 
 namespace ProyectoIPO2020_2021
 {
@@ -26,26 +26,53 @@ namespace ProyectoIPO2020_2021
         private VentanaAyuda ventanaAyuda = new VentanaAyuda();
         private Apadrinado ventanaPadrinos = new Apadrinado();
         
+        
 
-        private static BitmapImage imgUsuario1 = new BitmapImage(new Uri("/Images/Mike-Wazowski.jpg", UriKind.Relative));
-        private static Usuario usuario1 = new Usuario("Mario Torres Toledano", "Mario.Torres@gmail.com", "05285136F", 688455782, DateTime.Now, imgUsuario1, "123");
-        
-        public static OleDbConnection conectar = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\05980124\\OneDrive\\Uni\\Vs2019Projects\\ProyectoIPO2020-2021\\ProyectoIPO2020-2021\\BasesDeDatos\\BBDDPerros.accdb");
-        OleDbCommand comando = new OleDbCommand();
-        
         public Aplicacion()
         {
-            InitializeComponent();
             
+            InitializeComponent();
+            //cargarInfo();
+            /*
             txtblockDetUser.Text = usuario1.correo;
             imgPerfil.Source = usuario1.img;
             lblNombre.Content = usuario1.nombre;
             txtblockDni.Text = usuario1.dni;
             lblTlf.Content = usuario1.n_tlf;
             lblFecha.Content = usuario1.ult_acceso;
-
+            */
  
         }
+        /*private void cargarInfo()
+        {
+            if(Login.correo == usuario1.correo)
+            {
+                txtblockDetUser.Text = usuario1.correo;
+                imgPerfil.Source = usuario1.img;
+                lblNombre.Content = usuario1.nombre;
+                txtblockDni.Text = usuario1.dni;
+                lblTlf.Content = usuario1.n_tlf;
+                lblFecha.Content = usuario1.ult_acceso;
+            }
+            if (Login.correo == usuario2.correo)
+            {
+                txtblockDetUser.Text = usuario2.correo;
+                imgPerfil.Source = usuario2.img;
+                lblNombre.Content = usuario2.nombre;
+                txtblockDni.Text = usuario2.dni;
+                lblTlf.Content = usuario2.n_tlf;
+                lblFecha.Content = usuario2.ult_acceso;
+            }
+            if (Login.correo == usuario3.correo)
+            {
+                txtblockDetUser.Text = usuario3.correo;
+                imgPerfil.Source = usuario3.img;
+                lblNombre.Content = usuario3.nombre;
+                txtblockDni.Text = usuario3.dni;
+                lblTlf.Content = usuario3.n_tlf;
+                lblFecha.Content = usuario3.ult_acceso;
+            }
+        }*/
 
         private void btnAyuda_Click(object sender, RoutedEventArgs e)
         {
@@ -57,17 +84,14 @@ namespace ProyectoIPO2020_2021
             this.Close();
             
         }
+
         protected override void OnClosed(EventArgs e)
         {
             base.OnClosed(e);
 
             Application.Current.Shutdown();
         }
-        private void btnCbPerfil_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
+       
         private void btnConsultaApadrinado_Click(object sender, RoutedEventArgs e)
         {
             ventanaPadrinos.Show();
@@ -94,15 +118,381 @@ namespace ProyectoIPO2020_2021
 
         private void btnAñadir_Click(object sender, RoutedEventArgs e)
         {
-         
-           
+            if (string.IsNullOrEmpty(txtboxNombrePerro.Text))
+            {
+                txtboxNombrePerro.BorderBrush = Brushes.Red;
+                txtboxNombrePerro.Background = Brushes.LightSalmon;
+            }
+            else if (!(radiobMacho.IsChecked.Value))
+            {
+                if(!(radiobHembra.IsChecked.Value)){
+                    MessageBox.Show("No ha definido el sexo del perro ", "SexoPerro");
+                }
+                
+            }
+            else if (string.IsNullOrEmpty(txtboxRazaPerro.Text))
+            {
+                txtboxRazaPerro.BorderBrush = Brushes.Red;
+                txtboxRazaPerro.Background = Brushes.LightSalmon;
+            }
+            else if (string.IsNullOrEmpty(txtboxPesoPerro.Text))
+            {
+                txtboxPesoPerro.BorderBrush = Brushes.Red;
+                txtboxPesoPerro.Background = Brushes.LightSalmon;
+            }
+            else if (string.IsNullOrEmpty(txtEdadPerro.Text))
+            {
+                txtEdadPerro.BorderBrush = Brushes.Red;
+                txtEdadPerro.Background = Brushes.LightSalmon;
+            }
+            else if (string.IsNullOrEmpty(dateFechaPerro.Text))
+            {
+                dateFechaPerro.BorderBrush = Brushes.Red;
+                dateFechaPerro.Background = Brushes.LightSalmon;
+            }
+            else if (string.IsNullOrEmpty(txtboxDescripcion.Text))
+            {
+                txtboxDescripcion.BorderBrush = Brushes.Red;
+                txtboxDescripcion.Background = Brushes.LightSalmon;
+            }
+            else
+            {
+                string sexo_perro = obtenerSexoPerro();
+
+                StreamWriter escribir = new StreamWriter("TablaPerros.txt");
+
+                escribir.WriteLine("Nombre: " + txtboxNombrePerro.Text);
+                escribir.WriteLine("Sexo: " + sexo_perro);
+                escribir.WriteLine("Raza: " + txtboxRazaPerro.Text);
+                escribir.WriteLine("Peso: " + txtboxPesoPerro.Text);
+                escribir.WriteLine("Edad: " + txtEdadPerro.Text);
+                escribir.WriteLine("Fecha de entrada: " + dateFechaPerro.Text);
+                escribir.WriteLine("¿Apadrinado?: " + checkApadrinado.IsChecked);
+                escribir.WriteLine("Descripción: " + txtboxDescripcion.Text);
+                escribir.WriteLine("\n");
+                escribir.Close();
+                MessageBox.Show("El perro ha sido añadido ", "Perro");
+            }
+            
         }
-
-
 
         private void btnListar_Click(object sender, RoutedEventArgs e)
         {
+            richPerros.Document.Blocks.Clear();
+            StreamReader leer = new StreamReader("TablaPerros.txt");
+            string linea;
             
+            try
+            {
+                linea = leer.ReadLine();
+                while (linea != null)
+                {
+                    richPerros.AppendText(linea + "\n");
+                    linea = leer.ReadLine();
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private string obtenerSexoPerro()
+        {
+            string sexo;
+            if (radiobMacho == null)
+            {
+                sexo = (string)radiobHembra.Content;
+            }
+            else
+            {
+                sexo = (string)radiobMacho.Content;
+            }
+            return sexo;
+        }
+
+
+        private void CheckBoxLunes_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxLunesMa.IsEnabled = true;
+            CheckBoxLunesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxMartes_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMartesMa.IsEnabled = true;
+            CheckBoxMartesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxMiercoles_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMiercolesMa.IsEnabled = true;
+            CheckBoxMiercolesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxJueves_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxJuevesMa.IsEnabled = true;
+            CheckBoxJuevesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxViernes_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxViernesMa.IsEnabled = true;
+            CheckBoxViernesTar.IsEnabled = true;
+        }
+
+        private void CheckBoxSabado_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxSabadoMa.IsEnabled = true;
+            CheckBoxSabadoTar.IsEnabled = true;
+        }
+
+        private void CheckBoxDomingo_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxDomingoMa.IsEnabled = true;
+            CheckBoxDomingoTar.IsEnabled = true;
+        }
+
+        private void CheckBoxDomingo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxDomingoMa.IsEnabled = false;
+            CheckBoxDomingoTar.IsEnabled = false;
+            CheckBoxDomingoMa.IsChecked = false;
+            CheckBoxDomingoTar.IsChecked = false;
+
+        }
+
+        private void CheckBoxSabado_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxSabadoMa.IsEnabled = false;
+            CheckBoxSabadoTar.IsEnabled = false;
+            CheckBoxSabadoMa.IsChecked = false;
+            CheckBoxSabadoTar.IsChecked = false;
+        }
+
+        private void CheckBoxViernes_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxViernesMa.IsEnabled = false;
+            CheckBoxViernesTar.IsEnabled = false;
+            CheckBoxViernesMa.IsChecked = false;
+            CheckBoxViernesTar.IsChecked = false;
+        }
+
+        private void CheckBoxJueves_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxJuevesMa.IsEnabled = false;
+            CheckBoxJuevesTar.IsEnabled = false;
+            CheckBoxJuevesMa.IsChecked = false;
+            CheckBoxJuevesTar.IsChecked = false;
+        }
+
+        private void CheckBoxMiercoles_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMiercolesMa.IsEnabled = false;
+            CheckBoxMiercolesTar.IsEnabled = false;
+            CheckBoxMiercolesMa.IsChecked = false;
+            CheckBoxMiercolesTar.IsChecked = false;
+        }
+
+        private void CheckBoxMartes_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxMartesMa.IsEnabled = false;
+            CheckBoxMartesTar.IsEnabled = false;
+            CheckBoxMartesMa.IsChecked = false;
+            CheckBoxMartesTar.IsChecked = false;
+        }
+
+        private void CheckBoxLunes_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxLunesMa.IsEnabled = false;
+            CheckBoxLunesTar.IsEnabled = false;
+            CheckBoxLunesMa.IsChecked = false;
+            CheckBoxLunesTar.IsChecked = false;
+        }
+
+        private void txtboxNombreSocio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtboxApellidoSocio.Focus();
+            }
+        }
+
+        private void txtboxApellidoSocio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtboxDNISocio.Focus();
+            }
+        }
+
+        private void txtboxDNISocio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtboxTelefonoSocio.Focus();
+            }
+        }
+
+        private void txtboxTelefonoSocio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtCorreoElectronicoSocio.Focus();
+            }
+        }
+
+        private void txtCorreoElectronicoSocio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtEntidadBancaria.Focus();
+            }
+        }
+
+        private void txtEntidadBancaria_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtIBAN1.Focus();
+            }
+        }
+
+        private void txtIBAN1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtIBAN2.Focus();
+            }
+        }
+
+        private void txtIBAN2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtIBAN3.Focus();
+            }
+        }
+
+        private void txtIBAN3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtIBAN4.Focus();
+            }
+        }
+
+        private void txtIBAN4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtIBAN5.Focus();
+            }
+        }
+
+        private void txtIBAN5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtIBAN6.Focus();
+            }
+        }
+
+        private void txtIBAN6_keyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtCuantia.Focus();
+            }
+        }
+
+        private void txtCuantia_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                cboxformPago.Focus();
+            }
+        }
+
+        private void txtboxNombreVoluntario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtboxApellidoVoluntario.Focus();
+            }
+        }
+
+        private void txtboxApellidoVoluntario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtboxDNIVoluntario.Focus();
+            }
+        }
+
+        private void txtboxDNIVoluntario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtboxTelefonoVoluntario.Focus();
+            }
+        }
+
+        private void txtboxTelefonoVoluntario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtCorreoElectronicoVoluntario.Focus();
+            }
+        }
+
+        private void txtboxNombrePerro_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                radiobMacho.Focus();
+            }
+        }
+
+        private void radiobMacho_Checked(object sender, RoutedEventArgs e)
+        {
+            txtboxRazaPerro.Focus();   
+        }
+
+        private void radiobHembra_Checked(object sender, RoutedEventArgs e)
+        {
+            txtboxRazaPerro.Focus();
+        }
+
+        private void txtboxRazaPerro_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtboxPesoPerro.Focus();
+            }
+        }
+
+        private void txtboxPesoPerro_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txtEdadPerro.Focus();
+            }
+        }
+
+        private void txtEdadPerro_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                dateFechaPerro.Focus();
+            }
+        }
+
+        private void checkApadrinado_Checked(object sender, RoutedEventArgs e)
+        {
+            txtboxDescripcion.Focus();
         }
     }
 }
--- Ventanas/Login.xaml.cs
+++ Ventanas/Login.xaml.cs
@@ -28,27 +28,103 @@ namespace ProyectoIPO2020_2021
     {
         private Aplicacion ventanaApp = new Aplicacion();
 
+        private static BitmapImage imgUsuario1 = new BitmapImage(new Uri("/Images/Mike-Wazowski.jpg", UriKind.Relative));
+        private static BitmapImage imgUsuario2 = new BitmapImage(new Uri("/Images/Boo.png", UriKind.Relative));
+        private static BitmapImage imgUsuario3 = new BitmapImage(new Uri("/Images/Sulley.png", UriKind.Relative));
+
+        public static Usuario usuario1 = new Usuario("Mario Torres Toledano", "Mario.Torres@gmail.com", "05285136F", 688455782, DateTime.Now, imgUsuario1, "123");
+        public static Usuario usuario2 = new Usuario("Laura Castillo Rodriguez", "Laura.Castillo@gmail.com", "06742376C", 637328948, DateTime.Now, imgUsuario2, "123");
+        public static Usuario usuario3 = new Usuario("Ramon Llanos Espadas", "Ramon.LLanos@gmail.com", "03523428F", 698674651, DateTime.Now, imgUsuario3, "123");
+
+
         private BitmapImage imagCheck = new BitmapImage(new Uri("/Images/Check.jpg", UriKind.Relative));
         private BitmapImage imagCross = new BitmapImage(new Uri("/Images/Cross.jpg", UriKind.Relative));
         private BitmapImage imagOriginal = new BitmapImage(new Uri("/Images/icono.png", UriKind.Relative));
         private BitmapImage imagRollOver = new BitmapImage(new Uri("/Images/icono2.png", UriKind.Relative));
-        private AyudaContraseña ventanaAyudaContraseña = new AyudaContraseña();
-        private String correo = "Mario.Torres@gmail.com";
-        private String contra = "123";
         
+        public static string correo = "";
+        public static string contra = "";
+        private string correo1 =usuario1.correo;
+        private string contra1 = usuario1.contraseña;
+        private string correo2 = usuario2.correo;
+        private string contra2 = usuario2.contraseña;
+        private string correo3 = usuario3.correo;
+        private string contra3 = usuario3.contraseña;
+        
+
 
         public Login()
         {
+         
             InitializeComponent();
-            
+
         }
+
+        public string establecerCorreo()
+        {
+           
+            if (txtUsuario.Text == correo1)
+            {
+                correo = txtUsuario.Text;
+                ventanaApp.lblNombre.Content = usuario1.nombre;
+                ventanaApp.imgPerfil.Source = usuario1.img;
+                ventanaApp.txtblockDetUser.Text = correo;
+                ventanaApp.txtblockDni.Text = usuario1.dni;
+                ventanaApp.txtblockTelefenoUsuario.Text = usuario1.n_tlf.ToString();
+                ventanaApp.txtblockFecha.Text = usuario1.ult_acceso.ToString();
+
+
+            }
+            if(txtUsuario.Text == correo2)
+            {
+                correo = txtUsuario.Text;
+                ventanaApp.lblNombre.Content = usuario2.nombre;
+                ventanaApp.imgPerfil.Source = usuario2.img;
+                ventanaApp.txtblockDetUser.Text = correo;
+                ventanaApp.txtblockDni.Text = usuario2.dni;
+                ventanaApp.txtblockTelefenoUsuario.Text = usuario2.n_tlf.ToString();
+                ventanaApp.txtblockFecha.Text = usuario2.ult_acceso.ToString();
+            }
+            if(txtUsuario.Text == correo3)
+            {
+                correo = txtUsuario.Text;
+                ventanaApp.lblNombre.Content = usuario3.nombre;
+                ventanaApp.imgPerfil.Source = usuario3.img;
+                ventanaApp.txtblockDetUser.Text = correo;
+                ventanaApp.txtblockDni.Text = usuario3.dni;
+                ventanaApp.txtblockTelefenoUsuario.Text = usuario3.n_tlf.ToString();
+                ventanaApp.txtblockFecha.Text = usuario3.ult_acceso.ToString();
+            }
+        
+            return correo;
+        }
+
+        private string establecerContra()
+        {
+            if (passContra.Password == contra1)
+            {
+                contra = contra1;
+            }
+            if (passContra.Password == contra2)
+            {
+                contra = contra2;
+            }
+            if (passContra.Password == contra2)
+            {
+                contra = contra3;
+            }
+            return contra;
+        }
+
+
         private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
         {
             txtUsuario.IsEnabled = true;
             // se hará la comprobación al pulsar el "Enter"
             if (e.Key == Key.Return)
             {    
-                if (!String.IsNullOrEmpty(txtUsuario.Text) && String.IsNullOrEmpty(passContra.Password) && ComprobarEntrada(txtUsuario.Text, correo, txtUsuario, imgCheckUsuario))
+                //
+                if (!string.IsNullOrEmpty(txtUsuario.Text) && string.IsNullOrEmpty(passContra.Password) && ComprobarEntrada(txtUsuario.Text, establecerCorreo(), txtUsuario, imgCheckUsuario))
                 {
                     // habilitar entrada de contraseña y pasarle el foco
                     passContra.IsEnabled = true;
@@ -64,7 +140,7 @@ namespace ProyectoIPO2020_2021
                 if (e.Key == Key.Return)
                 {
 
-                    if (!String.IsNullOrEmpty(txtUsuario.Text) && !String.IsNullOrEmpty(passContra.Password) && ComprobarEntrada(txtUsuario.Text, correo, txtUsuario, imgCheckUsuario) && ComprobarEntrada(passContra.Password, contra, passContra, imgCheckContrasena))
+                    if (!string.IsNullOrEmpty(txtUsuario.Text) && !string.IsNullOrEmpty(passContra.Password) && ComprobarEntrada(txtUsuario.Text, establecerCorreo(), txtUsuario, imgCheckUsuario) && ComprobarEntrada(passContra.Password, establecerContra(), passContra, imgCheckContrasena))
                     {
                         ventanaApp.Show();
                     }
@@ -74,7 +150,7 @@ namespace ProyectoIPO2020_2021
         {
             // La comprobación ya lleva implícita que las entradas
             // estén vacías
-            if (ComprobarEntrada(txtUsuario.Text, correo,txtUsuario, imgCheckUsuario) && (ComprobarEntrada(passContra.Password, contra, passContra, imgCheckContrasena)))
+            if (ComprobarEntrada(txtUsuario.Text, establecerCorreo(), txtUsuario, imgCheckUsuario) && (ComprobarEntrada(passContra.Password, establecerContra(), passContra, imgCheckContrasena)))
             {
                 ventanaApp.Show();
             }
@@ -85,9 +161,9 @@ namespace ProyectoIPO2020_2021
             MessageBox.Show("¡Nos vemos! Hasta otra.", "Despedida");
         }
 
-        private Boolean ComprobarEntrada(string valorIntroducido, string valorValido, Control componenteEntrada, Image imagenFeedBack)
+        private bool ComprobarEntrada(string valorIntroducido, string valorValido, Control componenteEntrada, Image imagenFeedBack)
         {
-            Boolean valido = false;
+            bool valido = false;
             componenteEntrada.BorderThickness = new Thickness(2);
             if (valorIntroducido.Equals(valorValido))
             {
@@ -136,7 +212,12 @@ namespace ProyectoIPO2020_2021
 
         private void ContraseñaOlvidada_Click(object sender, RoutedEventArgs e)
         {
-            ventanaAyudaContraseña.Show();
+
+            MessageBox.Show("Si desea recuperar la contraseña mande un correo electrónico a: " +
+                "soporte.tecnico@prot3ctora.es indicando: \nSu correo electrónico, DNI y número de teléfono.\n" +
+                "Estableciendo como asunto HE OLVIDADO MI CONTRASEÑA. Se pondrán en contacto con usted lo antes posible.", 
+                "Cerrar");
+            //ventanaAyudaContraseña.Show();
         }
     }
 }
