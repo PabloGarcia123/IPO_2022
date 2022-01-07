863d7feb4107e041cab9e250704d382fbac54717
PabloGarcia123 <93787897+PabloGarcia123@users.noreply.github.com>
2022-01-07 23:14:25 +0100

    Add files via upload

--- Clases/Padrino.cs
+++ Clases/Padrino.cs
@@ -4,22 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProyectoIPO2020_2021.Clases
+namespace ProyectoIPO2020_2021
 {
-    class Padrino
+    public class Padrino
     {
         public string nombre { set; get; }
+        public string apellido { set; get; }
         public string dni { set; get; }
         public string correo { set; get; }
         public int n_tlf { set; get; }
         public int aportacion { set; get; }
         public string forma_pago { set; get; }
-        public int n_cuenta { set; get; }
+        public long n_cuenta { set; get; }
         public DateTime fecha_apadrinamiento { set; get; }
 
-        public Padrino(string nombre, string dni, string correo, int n_tlf, int aportacion, string forma_pago, int n_cuenta, DateTime fecha_apadrinamiento)
+        public Padrino(string nombre, string apellido, string dni, string correo, int n_tlf, int aportacion, string forma_pago, long n_cuenta, DateTime fecha_apadrinamiento)
         {
             this.nombre = nombre;
+            this.apellido = apellido;
             this.dni = dni;
             this.correo = correo;
             this.n_tlf = n_tlf;
